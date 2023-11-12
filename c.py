2a3dd727f7aa60c1cbfeace9e50e8c549579ba87
majugaliza <114781250+majugaliza@users.noreply.github.com>
2023-11-12 11:48:31 -0300

    Update c.py

--- c.py
+++ c.py
@@ -1,83 +1,78 @@
 class Pagina:
-    def __init__(self, numero):
-        self.numero = numero
-        self.conteudo = f"Conteudo da Pagina {numero}"
+    def init(self, tamanho_pagina):
+        self.infos = [None] * tamanho_pagina
 
-class MemoriaFisica:
-    def __init__(self, tamanho):
-        self.tamanho = tamanho
-        self.paginas = [None] * tamanho
 
-    def alocar_pagina(self, pagina):
-        for i in range(self.tamanho):
-            if self.paginas[i] is None:
-                self.paginas[i] = pagina
-                return i
-        return None
+class InfoTabela:
+    def init(self, numero_quadro=None, presente=False, modificada=False):
+        self.numero_quadro = numero_quadro
+        self.presente = presente
+        self.modificada = modificada
 
-    def substituir_pagina(self, pagina, pagina_substituida):
-        for i in range(self.tamanho):
-            if self.paginas[i] == pagina_substituida:
-                self.paginas[i] = pagina
-                return i
-        return None
 
-    def desalocar_pagina(self, indice):
-        self.paginas[indice] = None
+class TabelaPaginas:
+    def init(self, tamanho_pagina):
+        self.tamanho_pagina = tamanho_pagina
+        self.paginas = {}
 
-    def obter_pagina(self, indice):
-        return self.paginas[indice]
+    def mapear_pagina(self, endereco_virtual):
+        numero_pagina = endereco_virtual // self.tamanho_pagina
+        return self.paginas.get(numero_pagina)
 
-class GerenciadorMemoriaVirtual:
-    def __init__(self, tamanho_fisico, tamanho_virtual):
-        self.memoria_fisica = MemoriaFisica(tamanho_fisico)
-        self.tamanho_virtual = tamanho_virtual
-        self.tabela_paginas = [None] * tamanho_virtual
+    def carregar_pagina(self, numero_pagina, numero_quadro):
+        self.paginas[numero_pagina] = InfoTabela(numero_quadro=numero_quadro, presente=True, modificada=False)
 
-    def alocar_pagina(self, numero_pagina):
-        if self.tabela_paginas[numero_pagina] is None:
-            pagina = Pagina(numero_pagina)
-            indice_memoria_fisica = self.memoria_fisica.alocar_pagina(pagina)
-            if indice_memoria_fisica is not None:
-                self.tabela_paginas[numero_pagina] = indice_memoria_fisica
-                return True
-        return False
 
-    def acessar_pagina(self, numero_pagina):
-        if self.tabela_paginas[numero_pagina] is not None:
-            return self.tabela_paginas[numero_pagina]
-        else:
-            return None
+class MemoriaPrincipal:
+    def init(self, tamanho_total, tamanho_quadro):
+        self.tamanho_total = tamanho_total
+        self.tamanho_quadro = tamanho_quadro
+        self.quantidade_quadros = tamanho_total // tamanho_quadro
+        self.quadros = [None] * self.quantidade_quadros
 
-    def imprimir_estado(self):
-        print("Memória Física:")
-        for i, pagina in enumerate(self.memoria_fisica.paginas):
-            if pagina is not None:
-                print(f"Página {i}: Conteúdo={pagina.conteudo}")
-            else:
-                print(f"Página {i}: Vazia")
-        print("\nTabela de Páginas:")
-        for i, indice in enumerate(self.tabela_paginas):
-            if indice is not None:
-                print(f"Página Virtual {i} -> Página Física {indice}")
-            else:
-                print(f"Página Virtual {i} -> Não Alocada")
 
-# Simulação da execução de processos
-def simular_execucao(processos, gerenciador_memoria):
-    for processo in processos:
-        print(f"\nExecutando Processo {processo}:")
+class MS:
+    def init(self, tamanho_total, tamanho_pagina):
+        self.tamanho_total = tamanho_total
+        self.quantidade_paginas = tamanho_total // tamanho_pagina
+        self.paginas = [None] * self.quantidade_paginas
 
-        for pagina in range(5):  # Simula o acesso a 5 páginas em cada processo
-            numero_pagina = processo * 5 + pagina
-            if gerenciador_memoria.acessar_pagina(numero_pagina) is None:
-                if not gerenciador_memoria.alocar_pagina(numero_pagina):
-                    print(f"Falha de Página para Página Virtual {numero_pagina}")
+class GerenciadorMemoria:
+    def init(self, tamanho_ms, tamanho_memoria, tamanho_pagina):
+        self.disco = MS(tamanho_ms, tamanho_pagina)
+        self.memoria_principal = MemoriaPrincipal(tamanho_memoria, tamanho_pagina)
+        self.tabela_paginas = {}
+        self.lru = []
 
-        gerenciador_memoria.imprimir_estado()
+    def criar_processo(self, numero_processo, tamanho_imagem):
+        tabela_paginas = TabelaPaginas(tamanho_pagina=self.memoria_principal.tamanho_quadro)
+        self.tabela_paginas[numero_processo] = tabela_paginas
 
-# Exemplo de utilização
-gerenciador_virtual = GerenciadorMemoriaVirtual(tamanho_fisico=4, tamanho_virtual=20)
-processos_em_execucao = [0, 1, 2, 3, 4]
+        # Lógica para alocar quadros iniciais para o processo
+        # ...
 
-simular_execucao(processos_em_execucao, gerenciador_virtual)
+    def busca_mp(self, numero_processo, endereco):
+        
+        # Lógica para execução de operações de leitura na mp
+        # ...
+
+    def tratar_falta_pagina(self, numero_processo, numero_pagina):
+        
+        # Lógica para trazer a página da memória secundária para a memória principal
+        # ...
+
+    def busca_ms(self,numero_processo, endereco):
+
+        # Lógica para execução de operações de leitura na mp
+        # ...
+    
+
+def trabalho_so():
+    GM = GerenciadorMemoria(32768, 1024, 16)
+    
+    # Leitura do arquivo com lista de execução e chamada das funções do GM segundo a instrução solicitada
+    # ...
+
+
+if _name_ == "_main_":
+    trabalho_so()
