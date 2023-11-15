ef25f829ecdae946cda611961ebc84f8efb4d601
Gabriel0PG <143665429+Gabriel0PG@users.noreply.github.com>
2023-11-14 22:40:10 -0300

    Update gerenciador_de_memoria.py

--- gerenciador_de_memoria.py
+++ gerenciador_de_memoria.py
@@ -1,17 +1,19 @@
 class Pagina:
-    def init(self, tamanho_pagina):
+    def _init_(self, tamanho_pagina):
         self.infos = [None] * tamanho_pagina
+        for i in range(tamanho_pagina):
+            self.infos[i] = i
 
 
 class InfoTabela:
-    def init(self, numero_quadro=None, presente=False, modificada=False):
+    def _init_(self, numero_quadro=None, presente=False, modificada=False):
         self.numero_quadro = numero_quadro
         self.presente = presente
         self.modificada = modificada
 
 
 class TabelaPaginas:
-    def init(self, tamanho_pagina):
+    def _init_(self, tamanho_pagina):
         self.tamanho_pagina = tamanho_pagina
         self.paginas = {}
 
@@ -24,40 +26,66 @@ class TabelaPaginas:
 
 
 class MemoriaPrincipal:
-    def init(self, tamanho_total, tamanho_quadro):
+    def _init_(self, tamanho_total, tamanho_quadro):
         self.tamanho_total = tamanho_total
         self.tamanho_quadro = tamanho_quadro
         self.quantidade_quadros = tamanho_total // tamanho_quadro
         self.quadros = [None] * self.quantidade_quadros
 
+    def quadros_livres(self):
+        if self.quadros[-1] is not None:
+            return 0
+        for i in range(self.quantidade_quadros):
+            if self.quadros[i] is None:
+                return self.quantidade_quadros - i
 
-class MS:
-    def init(self, tamanho_total, tamanho_pagina):
+
+class MemoriaSecundaria:
+    def _init_(self, tamanho_total, tamanho_pagina):
         self.tamanho_total = tamanho_total
         self.quantidade_paginas = tamanho_total // tamanho_pagina
-        self.paginas = [None] * self.quantidade_paginas
+        self.processos = {}
+
+    def mapear_pagina(self, numero_processo, numero_pagina):
+        paginas = self.processos.get(numero_processo)
+        return paginas[numero_pagina]
+
+    def carregar_paginas(self, paginas, numero_processo):
+        self.processos[numero_processo] = paginas
+
 
 class GerenciadorMemoria:
-    def init(self, tamanho_ms, tamanho_memoria, tamanho_pagina):
-        self.disco = MS(tamanho_ms, tamanho_pagina)
-        self.memoria_principal = MemoriaPrincipal(tamanho_memoria, tamanho_pagina)
-        self.tabela_paginas = {}
+    def _init_(self, tamanho_ms, tamanho_memoria, tamanho_pagina):
+        self.ms = MemoriaSecundaria(tamanho_ms, tamanho_pagina)
+        self.mp = MemoriaPrincipal(tamanho_memoria, tamanho_pagina)
+        self.tabelas_paginas = {}
         self.lru = []
 
     def criar_processo(self, numero_processo, tamanho_imagem):
-        tabela_paginas = TabelaPaginas(tamanho_pagina=self.memoria_principal.tamanho_quadro)
-        self.tabela_paginas[numero_processo] = tabela_paginas
-
-        # Lógica para alocar quadros iniciais para o processo
-        # ...
+        tabela_paginas = TabelaPaginas(tamanho_pagina=self.mp.tamanho_quadro)
+        quantidade_de_paginas = tamanho_imagem // self.mp.tamanho_quadro
+        paginas = [None] * quantidade_de_paginas
+        for i in range(quantidade_de_paginas):
+            paginas[i] = Pagina(self.mp.tamanho_quadro)
+        quadros_livres = self.mp.quadros_livres()
+        if quadros_livres != 0:
+            if quadros_livres < quantidade_de_paginas:
+                for i, k in enumerate(range(self.mp.quantidade_quadros - quadros_livres - 1, self.mp.quantidade_quadros)):
+                    self.mp.quadros[k] = paginas[i]
+                    tabela_paginas.carregar_pagina(i, k)
+            else:
+                for i, k in enumerate(range(self.mp.quantidade_quadros - quadros_livres - 1, self.mp.quantidade_quadros)):
+                    self.mp.quadros[k] = paginas[i]
+                    tabela_paginas.carregar_pagina(i, k)
+        self.tabelas_paginas[numero_processo] = tabela_paginas
 
     def busca_mp(self, numero_processo, endereco):
-        
+
         # Lógica para execução de operações de leitura na mp
         # ...
 
     def tratar_falta_pagina(self, numero_processo, numero_pagina):
-        
+
         # Lógica para trazer a página da memória secundária para a memória principal
         # ...
 
@@ -65,14 +93,14 @@ class GerenciadorMemoria:
 
         # Lógica para execução de operações de leitura na mp
         # ...
-    
+
 
 def trabalho_so():
     GM = GerenciadorMemoria(32768, 1024, 16)
-    
+
     # Leitura do arquivo com lista de execução e chamada das funções do GM segundo a instrução solicitada
     # ...
 
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     trabalho_so()
